e1784ad2132507692a0b8257c317545fbb65a066
Amir Shehata <shehataa@ornl.gov>
2024-02-26 09:24:25 -0500

    DEFw: Add a setup for command line operation
    Bash script to setup environment variables for the command line
    
    Signed-off-by: Amir Shehata <amir.shehata@gmail.com>

---
+++ setup_cmdline.sh
@@ -0,0 +1,7 @@
+export DEFW_CONFIG_PATH=$HOME/ORNL/Quantum/QFw/defw/python/config/defw_cmdline.yaml
+export LD_LIBRARY_PATH=$HOME/ORNL/Quantum/QFw/defw/src/
+export DEFW_PATH=$HOME/ORNL/Quantum/QFw/defw
+export DEFW_AGENT_NAME=client
+export DEFW_LISTEN_PORT=8092
+export DEFW_AGENT_TYPE=agent
+export DEFW_LOG_LEVEL=message
